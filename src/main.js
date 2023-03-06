e03bbcd2bb6df951b417c2fd405b841586c916eb
Zalit0 <127117450+Zalit0@users.noreply.github.com>
2023-03-06 17:38:25 -0300

    Agregamos aos al main js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import { createPinia } from 'pinia';
 import './style.css';
 import App from './App.vue';
 import router from './router';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
+Aos.init();
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
