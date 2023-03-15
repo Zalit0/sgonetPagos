a0f19f2c0034b723d312fd01c6cdc6fe6b59e7d8
Zalit0 <alvarez.gonza86@gmail.com>
2023-03-15 00:57:51 -0300

    Seguimos modificando el apartado de abonos en el panel de administracion

--- src/components/TodosAbonos.vue
+++ src/components/TodosAbonos.vue
@@ -10,6 +10,28 @@ const guardar=()=>{
     editar.value= !editar.value
     console.log(props.abonos)
 }
+
+//agregamos un nuevo abono al array abonos
+const agregarAbono=()=>{
+  //preguntamos la cantidad de megas y la ponemos en el id
+  let id=prompt('De cuantos megas es el nuevo abono?');
+  //si el valor colocado es un numero continuamos 
+  if (!isNaN(id)){
+    id = id.trim()
+    const even = (element) => element.id === id;
+    //comprobamos si el Id ya existe
+    if(!props.abonos.some(even)){
+      props.abonos.push({id:id ,name:id+' Megas',precio:'',subida:'',bajada:''})
+    }
+    else{
+      alert("El id ya existe")
+    }
+  }
+  //si no es un numero alertamos
+  else{
+    alert('Debes colocar un numero')
+  }
+}
 </script>
 
 <template>
@@ -28,17 +50,17 @@ const guardar=()=>{
   </thead>
   <tbody>
     <tr v-for="(abono, index) in abonos" :key="abono.id">
-      <td><input type="text" class="form-control" v-model="abono.id" v-if="editar" :disabled="abono.id!=''"><span v-if="!editar">{{ abono.id }}</span></td>
-      <th  scope="row"><input type="text" class="form-control" v-model="abono.name" v-if="editar"><span v-if="!editar">{{ abono.name }}</span></th>
+      <td><input type="text" class="form-control" v-model="abono.id" v-if="editar" disabled><span v-if="!editar">{{ abono.id }}</span></td>
+      <th  scope="row"><input type="text" class="form-control" v-model="abono.name" v-if="editar" disabled><span v-if="!editar">{{ abono.name }}</span></th>
       <td><input type="text" class="form-control" v-model="abono.precio" v-if="editar"><span v-if="!editar">${{ abono.precio }}</span></td>
-      <td><input type="text" class="form-control" v-model="abono.bajada" v-if="editar"><span v-if="!editar">{{ abono.bajada }}</span></td>
-      <td><input type="text" class="form-control" v-model="abono.subida" v-if="editar"><span v-if="!editar">{{ abono.subida }}</span></td>
+      <td><input type="text" class="form-control" v-model="abono.bajada" v-if="editar"><span v-if="!editar">{{ abono.bajada }} MB</span></td>
+      <td><input type="text" class="form-control" v-model="abono.subida" v-if="editar"><span v-if="!editar">{{ abono.subida }} MB</span></td>
     </tr>
     </tbody>
   </table>
     <div class="btn-group" role="group" >
     <button class="btn btn-warning btn-sm" v-if="!editar" @click="editar= !editar">Editar</button>
-    <button class="btn btn-danger btn-sm text-center" v-if="editar" @click="abonos.push({id:'',name:'',precio:'',subida:'',bajada:''})">Agregar Abono</button> 
+    <button class="btn btn-danger btn-sm text-center" v-if="editar" @click="agregarAbono">Agregar Abono</button> 
     <button class="btn btn-warning btn-sm text-center" v-if="editar" @click="guardar">Guardar</button> 
     </div>
   </div>
